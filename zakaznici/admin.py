81b309436aa7e6d2cbfacde991207dc811998196
vrbaji1 <vrbaji1@uhk.cz>
2020-09-01 16:53:36 +0200

    Pridan zkusebni staticky obsah, upravy v administraci.

--- zakaznici/admin.py
+++ zakaznici/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Zakaznici, Ipv4
 
-admin.site.register(Zakaznici)
+class ZakazniciAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Jméno',           {'fields': ['jmeno','prijmeni']}),
+        ('Kontaktní údaje', {'fields': ['telefon','email']}),
+    ]
+
+admin.site.register(Zakaznici, ZakazniciAdmin)
 admin.site.register(Ipv4)
