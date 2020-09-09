98f6f3daf090b777e0ab79ea87f85eb9a67d4fa2
vrbaji1 <vrbaji1@uhk.cz>
2020-09-09 14:29:21 +0200

    Kulturnejsi admin rozhrani k zakaznikum a IPv4.

--- isp/admin.py
+++ isp/admin.py
@@ -3,11 +3,23 @@ from django.contrib import admin
 # Register your models here.
 from .models import Zakaznici, Ipv4
 
+
+
+class Ipv4Inline(admin.TabularInline):
+    model = Ipv4
+    #kolik se vzdy zobrazi volnych navic
+    extra = 1
+
+
 class ZakazniciAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Jméno',           {'fields': ['jmeno','prijmeni']}),
         ('Kontaktní údaje', {'fields': ['telefon','email']}),
     ]
+    inlines = [Ipv4Inline]
+    list_display = ('prijmeni', 'jmeno', 'telefon', 'email')
+    list_filter = ['prijmeni']
+    search_fields = ['prijmeni', 'jmeno', 'telefon', 'email']
 
 admin.site.register(Zakaznici, ZakazniciAdmin)
-admin.site.register(Ipv4)
+#admin.site.register(Ipv4)
