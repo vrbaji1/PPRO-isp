ca6d1ce13b4d5f15eed01b9e827f967ec402060a
vrbaji1 <vrbaji1@uhk.cz>
2020-10-12 17:50:07 +0200

    Vytvoreny tabulky Adresy, Tarify, Tarifni skupiny

--- isp/models.py
+++ isp/models.py
@@ -2,6 +2,35 @@ from django.db import models
 from django.core.validators import RegexValidator
 
 # Create your models here.
+
+
+class TarifniSkupiny(models.Model):
+    nazev = models.CharField(max_length=50)
+    def __str__(self):
+        return "%s" % (self.nazev)
+
+
+class Tarify(models.Model):
+    nazev = models.CharField(max_length=50)
+    cena = models.PositiveSmallIntegerField()
+    #kbit
+    rychlost_down = models.PositiveIntegerField()
+    rychlost_up = models.PositiveIntegerField()
+    id_tarifniskupiny = models.ForeignKey(TarifniSkupiny, on_delete=models.CASCADE)
+    def __str__(self):
+        return "%s" % (self.nazev)
+
+
+class Adresy(models.Model):
+    nazev_obce = models.CharField(max_length=50)
+    nazev_ulice = models.CharField(max_length=50)
+    cislo_domovni = models.PositiveSmallIntegerField()
+    psc = models.PositiveIntegerField()
+    id_tarifniskupiny = models.ForeignKey(TarifniSkupiny, on_delete=models.PROTECT)
+    def __str__(self):
+        return "%s %d, %s" % (self.nazev_ulice, self.cislo_domovni, self.nazev_obce)
+
+
 class Zakaznici(models.Model):
     tel_regex = RegexValidator(regex=r'^\+?\d{9,15}$', message="Zadejte telefon ve formatu +420123456789 nebo 123456789.")
     #
@@ -9,6 +38,8 @@ class Zakaznici(models.Model):
     prijmeni = models.CharField(max_length=50)
     telefon = models.CharField(validators=[tel_regex], max_length=16, blank=True)
     email = models.EmailField(max_length=50)
+    id_adresy = models.ForeignKey(Adresy, on_delete=models.PROTECT, null=True, default=None)
+    id_tarifu = models.ForeignKey(Tarify, on_delete=models.PROTECT, null=True, default=None)
     def __str__(self):
         return "%s %s" % (self.jmeno, self.prijmeni)
     def ma_email(self):
